41ff4a7d949f7ac97ae3f03a42fc5760165efb3e
RickFeith <feith.rick@gmail.com>
2018-09-22 22:29:47 +0200

    working env with  networks as agents. ,added large cnn

--- networkClass.py
+++ networkClass.py
@@ -5,12 +5,13 @@ class agent:
     """
     Provides a network model that is used for determining actions based on the observations
     Also provides training functionality
-    nCNNLayers: the number of CNN layers
-    nFCLayers: the number of fully connected layers
+    CNNoption: the network that is selected
+        large: consists of ...
+        small: consists of ...
 
     """
 
-    def __init__(self, env, sess, nCNNLayers, nFCLayers):
+    def __init__(self, env, sess, CNNoption):
         self.env = env
 
 
@@ -18,40 +19,65 @@ class agent:
             class network:
                 def __init__(self):
                     self.env = env
-                    self.nCNNLayers = nCNNLayers
-                    self.nFCLayers = nFCLayers
+                    self.CNNoption = CNNoption
                     self.sess = sess
                     self.shp = self.env.observation_space.shape
                     self.X = tf.placeholder(tf.float32,shape=[None, self.shp[0],self.shp[1],self.shp[2]], name = "Observation")
                     self.outputShape = self.env.action_space.n
-                    self.actions=self.cnn()
+                    if self.CNNoption == 'small':
+                        self.actions=self.cnnSmall()
+                    elif self.CNNoption == 'large':
+                        self.actions=self.cnnLarge()
+                    else:
+                        print('Invalid network option')
+                    self.sess.run(tf.global_variables_initializer())
 
-                def conv(self, input, outputDepth, kernelsize, stride, padding="same", activation_fn=tf.nn.relu, name = 'Conv'):
+                    ## function that creates a convolution layers
+                def conv(self, input, outputDepth, kernelsize, stride, padding="valid", activation_fn=tf.nn.relu, name = 'Conv'):
                       return tf.layers.conv2d(
                       inputs=input,
                       filters=outputDepth,
                       kernel_size=[kernelsize, kernelsize],strides=(stride,stride),padding=padding,activation=activation_fn, name = name)
 
-                def fc(self, input, numOutputs, activation_fn=tf.nn.relu):
-                    tf.layers.dense(
+                      ##Function that creates a fully connected layer
+                def fc(self, input, numOutputs, activation_fn=tf.nn.relu, name = 'FC'):
+                    return tf.layers.dense(
                         input,
                         numOutputs,
-                        activation=activation_fn, name = 'FC')
+                        activation=activation_fn, name = name)
 
-                def cnn(self):
+                    ## building cnnsmall
+                def cnnSmall(self):
                     print(self.X.shape)
                     outputC1 = self.conv(self.X, 8, 8, 4, name = 'Conv1')
                     print(outputC1.shape)
                     outputC2 = self.conv(outputC1, 16, 4, 2, name = 'Conv2')
                     print(outputC2.shape)
+                    outputFlatten = tf.layers.flatten(outputC2)
+                    print(outputFlatten.shape)
                     print(self.outputShape)
-                    outputFC = self.fc(outputC2, self.outputShape)
-                    print(outputFC)
+                    outputFC = self.fc(outputFlatten, self.outputShape, name= 'FC')
+                    print(outputFC.shape)
                     return outputFC
 
+                    ## building cnnlarge
+                def cnnLarge(self):
+                    print(self.X.shape)
+                    outputC1 = self.conv(self.X, 32, 8, 4, name = 'Conv1')
+                    print(outputC1.shape)
+                    outputC2 = self.conv(outputC1, 64, 4, 2, name = 'Conv2')
+                    print(outputC2.shape)
+                    outputC3 = self.conv(outputC1, 64, 3, 1, name = 'Conv3')
+                    outputFlatten = tf.layers.flatten(outputC3)
+                    print(outputFlatten.shape)
+                    print(self.outputShape)
+                    outputFC = self.fc(outputFlatten, self.outputShape, name= 'FC')
+                    print(outputFC.shape)
+                    return outputFC
 
+                    ##step function that can be called to get an action space output from the actor network
                 def step(self, observation):
-                    return self.sess.run(self.actions, feed_dict= {self.X : observation.reshape(1, self.shp[0],self.shp[1],self.shp[2])})
+                    return self.sess.run(self.actions, feed_dict= {self.X : observation})
 
             return network()
 
--- run.py
+++ run.py
@@ -6,16 +6,18 @@ import cv2
 import numpy as np
 from gymClass import *
 from networkClass import *
+import time
+import timeit
 ## parsing function for easy running
 def parse_args():
         parser = argparse.ArgumentParser(description='Plot results of Simulations')
-        parser.add_argument('--env', default="SpaceInvaders-v0")
+        parser.add_argument('--env', default="SpaceInvadersDeterministic-v4")
         parser.add_argument('--resultsPath', default="results/")
         parser.add_argument('--play', action='store_true')
         parser.add_argument('--stacks', default=4, type=int)
-        parser.add_argument('--numSteps', default=3, type=int)
-        parser.add_argument('--cnn', default=4, type=int)
-        parser.add_argument('--fc', default=4, type=int)
+        parser.add_argument('--numSteps', default=1000, type=int)
+        parser.add_argument('--CNNoption', default='small', type=str)
+        # parser.add_argument('--fc', default=4, type=int)
         args = parser.parse_args()
         return args
 
@@ -27,12 +29,12 @@ env = gym.make(args.env)
 env = gym.wrappers.Monitor(env, args.resultsPath, force=True)
 env = adjustFrame(env)
 env = stackFrames(env, args.stacks)
+print(env.unwrapped.get_action_meanings())
 
 ##create network
 sess = tf.Session()
-
-runner = agent(env, sess, args.cnn, args.fc)
-
+runner = agent(env, sess, args.CNNoption)
+writer = tf.summary.FileWriter("output", sess.graph)
 
 ##reset enviroment
 obs = env.reset()
@@ -41,13 +43,30 @@ obs = env.reset()
 Rewards = []
 
 ##main loop
-for _ in range(args.numSteps):
+
+t = time.time()
+d = 0
+for i in range(args.numSteps):
     env.render()
-    action = runner.step(obs)
-    print(action)
-    # obs, reward, done, info = env.step(action)
-    # if done: obs = env.reset()
-    # Rewards.append(reward)
-print(action.dtype)
+    lt = time.time()
+    # if i %100 ==0:
+    #     plt.imshow(obs.reshape((84,84*4),order='F'))
+    #     plt.show()
+    action = runner.step(obs.reshape((1,84,84,4)))
+    # print(time.time()-lt)
+    # print(action)
+    # action = np.random.random(size=(6))
+    action = np.argmax(action)
+    obs, reward, done, info = env.step(action)
+    if done:
+        obs = env.reset()
+        d +=1
+    Rewards.append(action)
+    # print(obs.shape)
+ttime = time.time()-t
+print('number of played eps: ', d)
+print(args.numSteps)
+print("fps: ", args.numSteps/(ttime))
 env.env.env.env.close()
+writer.close()
 sess.close()
