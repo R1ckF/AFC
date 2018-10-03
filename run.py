4386c0a2dc5c0f8a79a98e63ac3f426c17d878dd
Rick <feith.rick@gmail.com>
2018-10-03 09:11:55 +0200

    fr

--- run.py
+++ run.py
@@ -30,7 +30,7 @@ def parse_args():
         parser.add_argument('--nMiniBatch', default = 4, type=int, help = 'number of minibatches per trainingepoch')
         parser.add_argument('--loadPath', default = None, help = 'Load existing model')
         parser.add_argument('--saveInterval', default = 100000, type=int, help = 'save current network to disk')
-        parser.add_argument('--logInterval', default = 1000, type=int, help = 'print Log message')
+        parser.add_argument('--logInterval', default = 10000, type=int, help = 'print Log message')
         parser.add_argument('--cnnStyle', default = 'copy', help = 'copy for 2 CNN and seperate FC layers, shared for shared CNN but seperate FC layers')
         parser.add_argument('--lamda', default = 0.95, help = 'GAE from PPO article')
         parser.add_argument('--c1', default = 1, help = 'VF coefficient')
